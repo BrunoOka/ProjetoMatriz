78dbf403c4550900f7dae8da1fbf80ab157e5bb3
bruno <brunohideki31@gmail.com>
2025-11-19 12:12:51 -0300

    começando frm4

--- PROJETO-MATRIZ/Form4.Designer.cs
+++ PROJETO-MATRIZ/Form4.Designer.cs
@@ -28,7 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtnum = new System.Windows.Forms.TextBox();
+            this.txtpesquisa = new System.Windows.Forms.TextBox();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.lblnumero = new System.Windows.Forms.Label();
+            this.lblpesquisa = new System.Windows.Forms.Label();
+            this.lbla = new System.Windows.Forms.Label();
+            this.lblb = new System.Windows.Forms.Label();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnpesquisar = new System.Windows.Forms.Button();
+            this.btnlimpar = new System.Windows.Forms.Button();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.btncalcular = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtnum
+            // 
+            this.txtnum.Location = new System.Drawing.Point(177, 103);
+            this.txtnum.Name = "txtnum";
+            this.txtnum.Size = new System.Drawing.Size(100, 20);
+            this.txtnum.TabIndex = 0;
+            // 
+            // txtpesquisa
+            // 
+            this.txtpesquisa.Location = new System.Drawing.Point(175, 161);
+            this.txtpesquisa.Name = "txtpesquisa";
+            this.txtpesquisa.Size = new System.Drawing.Size(100, 20);
+            this.txtpesquisa.TabIndex = 1;
+            // 
+            // txta
+            // 
+            this.txta.Location = new System.Drawing.Point(359, 83);
+            this.txta.Multiline = true;
+            this.txta.Name = "txta";
+            this.txta.ReadOnly = true;
+            this.txta.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txta.Size = new System.Drawing.Size(100, 211);
+            this.txta.TabIndex = 2;
+            // 
+            // txtb
+            // 
+            this.txtb.Location = new System.Drawing.Point(546, 83);
+            this.txtb.Multiline = true;
+            this.txtb.Name = "txtb";
+            this.txtb.ReadOnly = true;
+            this.txtb.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtb.Size = new System.Drawing.Size(100, 211);
+            this.txtb.TabIndex = 3;
+            // 
+            // lblnumero
+            // 
+            this.lblnumero.AutoSize = true;
+            this.lblnumero.Location = new System.Drawing.Point(127, 109);
+            this.lblnumero.Name = "lblnumero";
+            this.lblnumero.Size = new System.Drawing.Size(42, 13);
+            this.lblnumero.TabIndex = 4;
+            this.lblnumero.Text = "numero";
+            // 
+            // lblpesquisa
+            // 
+            this.lblpesquisa.AutoSize = true;
+            this.lblpesquisa.Location = new System.Drawing.Point(120, 168);
+            this.lblpesquisa.Name = "lblpesquisa";
+            this.lblpesquisa.Size = new System.Drawing.Size(49, 13);
+            this.lblpesquisa.TabIndex = 5;
+            this.lblpesquisa.Text = "pesquisa";
+            // 
+            // lbla
+            // 
+            this.lbla.AutoSize = true;
+            this.lbla.Location = new System.Drawing.Point(390, 312);
+            this.lbla.Name = "lbla";
+            this.lbla.Size = new System.Drawing.Size(13, 13);
+            this.lbla.TabIndex = 6;
+            this.lbla.Text = "a";
+            // 
+            // lblb
+            // 
+            this.lblb.AutoSize = true;
+            this.lblb.Location = new System.Drawing.Point(581, 312);
+            this.lblb.Name = "lblb";
+            this.lblb.Size = new System.Drawing.Size(13, 13);
+            this.lblb.TabIndex = 7;
+            this.lblb.Text = "b";
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(130, 354);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(75, 23);
+            this.btnadd.TabIndex = 8;
+            this.btnadd.Text = "adicionar";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // btnpesquisar
+            // 
+            this.btnpesquisar.Location = new System.Drawing.Point(177, 207);
+            this.btnpesquisar.Name = "btnpesquisar";
+            this.btnpesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btnpesquisar.TabIndex = 9;
+            this.btnpesquisar.Text = "pesquisar";
+            this.btnpesquisar.UseVisualStyleBackColor = true;
+            this.btnpesquisar.Visible = false;
+            this.btnpesquisar.Click += new System.EventHandler(this.btnpesquisar_Click);
+            // 
+            // btnlimpar
+            // 
+            this.btnlimpar.Location = new System.Drawing.Point(329, 354);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnlimpar.TabIndex = 10;
+            this.btnlimpar.Text = "limpar";
+            this.btnlimpar.UseVisualStyleBackColor = true;
+            // 
+            // btnsair
+            // 
+            this.btnsair.Location = new System.Drawing.Point(525, 354);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(75, 23);
+            this.btnsair.TabIndex = 11;
+            this.btnsair.Text = "sair";
+            this.btnsair.UseVisualStyleBackColor = true;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(130, 354);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.TabIndex = 12;
+            this.btncalcular.Text = "calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Visible = false;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
             // 
             // frm4
             // 
@@ -36,13 +169,41 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.ControlBox = false;
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.btnsair);
+            this.Controls.Add(this.btnlimpar);
+            this.Controls.Add(this.btnpesquisar);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.lblb);
+            this.Controls.Add(this.lbla);
+            this.Controls.Add(this.lblpesquisa);
+            this.Controls.Add(this.lblnumero);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.txtpesquisa);
+            this.Controls.Add(this.txtnum);
             this.Name = "frm4";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "3";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtnum;
+        private System.Windows.Forms.TextBox txtpesquisa;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.Label lblnumero;
+        private System.Windows.Forms.Label lblpesquisa;
+        private System.Windows.Forms.Label lbla;
+        private System.Windows.Forms.Label lblb;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btnpesquisar;
+        private System.Windows.Forms.Button btnlimpar;
+        private System.Windows.Forms.Button btnsair;
+        private System.Windows.Forms.Button btncalcular;
     }
 }
--- PROJETO-MATRIZ/Form4.cs
+++ PROJETO-MATRIZ/Form4.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,52 @@ namespace PROJETO_MATRIZ
         public frm4()
         {
             InitializeComponent();
+        }
+        Double[] num = new double[30];
+        Double[] cubo = new double[30];
+        int i = 0;
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            if (i < 30)
+            {
+                num[i] = Convert.ToDouble(txtnum.Text);
+                txta.Text += Convert.ToString(num[i]) + Environment.NewLine;
+                i++;
+                txtnum.Clear();
+                txtnum.Focus();
+                
+            }
+            else 
+            {
+                MessageBox.Show("Matriz cheia!");
+                btnadd.Hide();
+                btnpesquisar.Show();
+                btncalcular.Show();
+                i = 0;
+            }
+        }
+
+        private void btnpesquisar_Click(object sender, EventArgs e)
+        {
+            int valor = Convert.ToInt32(txtpesquisa.Text);
+            bool encontrou = false;
+
+            for (int i = 0; i < 30; i++) { }
+            if (c)
+            {
+                encontrou = false;
+            }
+        }
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            while (i < 30)
+            {
+                cubo[i] = Math.Pow(num[i], 3);
+                txtb.Text += cubo[i] + Environment.NewLine;
+                i++;
+            }
         }
     }
 }
